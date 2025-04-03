071dbf598ce9ffd1b2e395097256545423c74943
David Celis <me@davidcel.is>
2025-04-03 14:57:03 -0700

    Initial commit

---
+++ lib/block_kit.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require_relative "block_kit/version"
+
+module BlockKit
+end
---
+++ lib/block_kit/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module BlockKit
+  VERSION = "0.1.0"
+end
