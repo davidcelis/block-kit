ac47931d42b0d54c1ef64f68d61b04e10628b8d3
David Celis <me@davidcel.is>
2025-04-13 17:03:48 -0700

    Add DSL methods for building Context blocks

--- lib/block_kit/layout/context.rb
+++ lib/block_kit/layout/context.rb
@@ -15,6 +15,28 @@ module BlockKit
 
       validates :elements, presence: true, length: {maximum: 10, message: "is too long (maximum is %{count} elements)"}, "block_kit/validators/associated": true
 
+      def image(alt_text:, image_url: nil, slack_file: nil)
+        if (image_url.nil? && slack_file.nil?) || (image_url && slack_file)
+          raise ArgumentError, "Must provide either image_url or slack_file, but not both."
+        end
+
+        append(Elements::Image.new(alt_text: alt_text, image_url: image_url, slack_file: slack_file))
+      end
+
+      def mrkdwn(text:, verbatim: nil)
+        append(Composition::Mrkdwn.new(text: text, verbatim: verbatim))
+      end
+
+      def plain_text(text:, emoji: nil)
+        append(Composition::PlainText.new(text: text, emoji: emoji))
+      end
+
+      def append(element)
+        elements << element
+
+        self
+      end
+
       def as_json(*)
         super.merge(elements: elements&.map(&:as_json)).compact
       end
--- spec/block_kit/layout/context_spec.rb
+++ spec/block_kit/layout/context_spec.rb
@@ -14,6 +14,94 @@ RSpec.describe BlockKit::Layout::Context, type: :model do
     }
   end
 
+  describe "#image" do
+    let(:args) { {alt_text: "A beautiful image", image_url: "https://example.com/image.png"} }
+    subject { block.image(**args) }
+
+    it "appends an image element" do
+      expect { subject }.to change { block.elements.size }.by(1)
+      expect(subject).to eq(block)
+
+      expect(block.elements.last).to be_a(BlockKit::Elements::Image)
+      expect(block.elements.last.alt_text).to eq("A beautiful image")
+      expect(block.elements.last.image_url).to eq("https://example.com/image.png")
+    end
+
+    context "with slack_file" do
+      let(:args) { {alt_text: "A beautiful image", slack_file: BlockKit::Composition::SlackFile.new} }
+
+      it "passes args to the image" do
+        expect { subject }.to change { block.elements.size }.by(1)
+
+        expect(block.elements.last.alt_text).to eq("A beautiful image")
+        expect(block.elements.last.slack_file).to eq(args[:slack_file])
+        expect(block.elements.last.image_url).to be_nil
+      end
+    end
+
+    context "with both image_url and slack_file" do
+      let(:args) { {alt_text: "A beautiful image", image_url: "https://example.com/image.png", slack_file: BlockKit::Composition::SlackFile.new} }
+
+      it "raises an ArgumentError" do
+        expect { subject }.to raise_error(ArgumentError, "Must provide either image_url or slack_file, but not both.")
+      end
+    end
+
+    context "with neither image_url nor slack_file" do
+      let(:args) { {alt_text: "A beautiful image"} }
+
+      it "raises an ArgumentError" do
+        expect { subject }.to raise_error(ArgumentError, "Must provide either image_url or slack_file, but not both.")
+      end
+    end
+  end
+
+  describe "#mrkdwn" do
+    let(:args) { {text: "This is a text element"} }
+    subject { block.mrkdwn(**args) }
+
+    it "appends a mrkdwn element" do
+      expect { subject }.to change { block.elements.size }.by(1)
+      expect(subject).to eq(block)
+
+      expect(block.elements.last).to be_a(BlockKit::Composition::Mrkdwn)
+      expect(block.elements.last.text).to eq("This is a text element")
+    end
+
+    context "with optional args" do
+      let(:args) { super().merge(verbatim: true) }
+
+      it "passes args to the mrkdwn" do
+        expect { subject }.to change { block.elements.size }.by(1)
+
+        expect(block.elements.last.verbatim).to eq(true)
+      end
+    end
+  end
+
+  describe "#plain_text" do
+    let(:args) { {text: "Hello"} }
+    subject { block.plain_text(**args) }
+
+    it "appends a plain_text element" do
+      expect { subject }.to change { block.elements.size }.by(1)
+      expect(subject).to eq(block)
+
+      expect(block.elements.last).to be_a(BlockKit::Composition::PlainText)
+      expect(block.elements.last.text).to eq("Hello")
+    end
+
+    context "with optional args" do
+      let(:args) { super().merge(emoji: true) }
+
+      it "passes args to the plain_text" do
+        expect { subject }.to change { block.elements.size }.by(1)
+
+        expect(block.elements.last.emoji).to eq(true)
+      end
+    end
+  end
+
   describe "#as_json" do
     it "serializes to JSON" do
       expect(block.as_json).to eq({
