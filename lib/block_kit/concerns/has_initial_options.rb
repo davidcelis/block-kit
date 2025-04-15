9801564306f41fafd15d11e0af1a0e6db426adf7
David Celis <me@davidcel.is>
2025-04-15 16:55:01 -0700

    Don't validate only one initial option, just return the last

--- lib/block_kit/concerns/has_initial_options.rb
+++ lib/block_kit/concerns/has_initial_options.rb
@@ -4,15 +4,19 @@ module BlockKit
   module Concerns
     module HasInitialOptions
       def initial_options
-        if respond_to?(:option_groups)
-          options&.select(&:initial?) || option_groups&.flat_map(&:options)&.select(&:initial?)
-        else
-          options&.select(&:initial?)
-        end
+        all_options&.select(&:initial?)
       end
 
       def as_json(*)
         super.merge(initial_options: initial_options&.map(&:as_json).presence).compact
+      end
+
+      private
+
+      def all_options
+        opts = Array(options)
+        opts += Array(option_groups).flat_map(&:options) if respond_to?(:option_groups)
+        opts
       end
     end
   end
--- spec/support/shared_examples/has_initial_option.rb
+++ spec/support/shared_examples/has_initial_option.rb
@@ -25,33 +25,28 @@ RSpec.shared_examples_for "a block that has one initial option" do
     end
   end
 
-  it "validates that only one option can be initial" do
-    subject.options = [
-      {text: "Option 1", value: "option_1", initial: true},
-      {text: "Option 2", value: "option_2"},
-      {text: "Option 3", value: "option_3", initial: true}
-    ]
-
-    expect(subject).not_to be_valid
-    expect(subject.errors["options[0]"]).to include("can't be initial when other options are also as initial")
-    expect(subject.errors["options[1]"]).to be_empty
-    expect(subject.errors["options[2]"]).to include("can't be initial when other options are also as initial")
-
-    # Test that the validation works with option groups as well
-    if subject.respond_to?(:option_groups)
-      subject.option_groups = [
-        {label: "Group 1", options: [{text: "Option A", value: "option_a", initial: true}]},
-        {label: "Group 2", options: [{text: "Option B", value: "option_b"}]},
-        {label: "Group 3", options: [{text: "Option C", value: "option_c", initial: true}]}
+  describe "#initial_option" do
+    it "returns whatever was marked as initial last" do
+      subject.options = [
+        {text: "Option 1", value: "option_1", initial: true},
+        {text: "Option 2", value: "option_2"},
+        {text: "Option 3", value: "option_3", initial: true}
       ]
 
-      expect(subject).not_to be_valid
-      expect(subject.errors["options[0]"]).to include("can't be initial when other options are also as initial")
-      expect(subject.errors["options[1]"]).to be_empty
-      expect(subject.errors["options[2]"]).to include("can't be initial when other options are also as initial")
-      expect(subject.errors["option_groups[0].options[0]"]).to include("can't be initial when other options are also as initial")
-      expect(subject.errors["option_groups[1].options[0]"]).to be_empty
-      expect(subject.errors["option_groups[2].options[0]"]).to include("can't be initial when other options are also as initial")
+      expect(subject.initial_option.text.text).to eq("Option 3")
+      expect(subject.initial_option.value).to eq("option_3")
+
+      # Test that the validation works with option groups as well
+      if subject.respond_to?(:option_groups)
+        subject.option_groups = [
+          {label: "Group 1", options: [{text: "Option A", value: "option_a", initial: true}]},
+          {label: "Group 2", options: [{text: "Option B", value: "option_b"}]},
+          {label: "Group 3", options: [{text: "Option C", value: "option_c", initial: true}]}
+        ]
+
+        expect(subject.initial_option.text.text).to eq("Option C")
+        expect(subject.initial_option.value).to eq("option_c")
+      end
     end
   end
 end
