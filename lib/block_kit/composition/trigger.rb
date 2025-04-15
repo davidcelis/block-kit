29609fc18b44b048560855c6fd9bee3065aa3ed2
David Celis <me@davidcel.is>
2025-04-15 16:04:06 -0700

    Add fixers to workflow triggers

--- lib/block_kit/composition/trigger.rb
+++ lib/block_kit/composition/trigger.rb
@@ -5,12 +5,16 @@ require "uri"
 module BlockKit
   module Composition
     class Trigger < Block
+      MAX_URL_LENGTH = 3000
+
       self.type = :trigger
 
       attribute :url, :string
       attribute :customizable_input_parameters, Types::Array.of(Composition::InputParameter)
 
-      validates :url, presence: true, format: {with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), message: "is not a valid URI", allow_blank: true}
+      validates :url, presence: true, length: {maximum: MAX_URL_LENGTH}, format: {with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), message: "is not a valid URI", allow_blank: true}
+      fixes :url, truncate: {maximum: MAX_URL_LENGTH}, null_value: {error_types: [:blank, :invalid]}
+
       validates :customizable_input_parameters, presence: true, "block_kit/validators/associated": true, allow_nil: true
 
       dsl_method :customizable_input_parameters, as: :customizable_input_parameter, required_fields: [:name, :value], yields: false
